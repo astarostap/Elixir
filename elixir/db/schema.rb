73d194b92b3e5f7318ab1a14d1011d7b84c1f67f
Abraham <abraham_starosta@dn0a232407.sunet>
2015-05-10 14:45:05 -0700

    last

--- elixir/db/schema.rb
+++ elixir/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150505045552) do
-=======
 ActiveRecord::Schema.define(version: 20150505061306) do
->>>>>>> carousel
 
   create_table "agrees", force: true do |t|
     t.datetime "created_at"
