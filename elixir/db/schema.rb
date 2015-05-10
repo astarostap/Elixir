a2754ad1a8ca8cb052efe8bb0cbdd1ede5d3080d
John Ekins <jpekins@gmail.com>
2015-05-10 14:26:49 -0700

    carousel files

--- elixir/db/schema.rb
+++ elixir/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426205814) do
+ActiveRecord::Schema.define(version: 20150505061306) do
 
   create_table "agrees", force: true do |t|
     t.datetime "created_at"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150426205814) do
     t.string   "option2"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_link"
   end
 
   create_table "responses", force: true do |t|
