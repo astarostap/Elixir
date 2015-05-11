940913f82a9f72542fee58c25d55729a496312de
John Ekins <jpekins@gmail.com>
2015-05-10 18:17:38 -0700

    carousel

--- elixir/app/controllers/questions_controller.rb
+++ elixir/app/controllers/questions_controller.rb
@@ -1,4 +1,12 @@
 class QuestionsController < ApplicationController
+	def show
+		/q_id = params[:qid]
+		@active_question = questions.find(q_id)/
+		text = "omg this is a really long and interesting question and I love pie so much. God I wish I had some pie right now, I would be so happy...."
+		o1 = "this is a really long option 1 but I want to test if it wraps correctly"
+		@question = Question.new(:title => "Title", :text => text, :option1 => o1, :option2 => "Option 2")
+	end
+
 	def index
 		@all_questions = Question.all
 		@first, *@rest = @all_questions
--- elixir/app/controllers/users_controller.rb
+++ elixir/app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
 		else
 			@new_user.password=(f[:password])
 			if @new_user.save
-				#session[:id] = @new_user.id
-				#session[:first_name] = @new_user.username
+				session[:id] = @new_user.id
+				session[:username] = @new_user.username
+				flash.notice = "Welcome " + @user.username + "!"
 				redirect_to :controller => "questions"
 			else
 				flash.notice = "Didn't save the user"
@@ -30,26 +31,32 @@ class UsersController < ApplicationController
 
 	def post_login
 		f = params[:user]
-		if not User.find_by_login(f[:username]) 
+		if not NormalUser.find_by_username(f[:username]) 
 			flash.notice = "Wrong username or password, try again!"
 			redirect_to :controller => "users",  :action => "login"
 
 		else
 		
-			@user = User.find_by_login(f[:username])
+			@user = NormalUser.find_by_username(f[:username])
+			puts(@user)
 
 			if not @user.password_valid(f[:password])
-				flash.notice = "Wrong login or password, try again!"
+				flash.notice = "Wrong username or password, try again!"
 				redirect_to :controller => "users",  :action => "login"
 			else
-                #@user_id = @user.id
-				#session[:id] = @user_id
-				#session[:first_name] = @user.first_name
-				#session[:last_name] = @user.last_name
-				flash.notice = "Welcome " + @user.first_name + "!"
-				redirect_to :controller => "users",  :action => "login"
+                @user_id = @user.id
+				session[:id] = @user_id
+				session[:username] = @user.username
+				flash.notice = "Welcome " + @user.username + "!"
+				redirect_to :controller => "questions"
 			end
 
 		end
+	end
+
+	def logout
+		reset_session
+		flash.notice = "You have logged out. See you soon!"
+		redirect_to :controller => "users", :action => "login"
 	end
 end
