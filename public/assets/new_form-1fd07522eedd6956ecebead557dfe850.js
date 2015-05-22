923a246e4ceea0f9787f8a99ca5cf2b960243f38
Abraham Starosta <starosta@stanford.com>
2015-05-21 23:00:47 -0700

    asd

---
+++ public/assets/new_form-1fd07522eedd6956ecebead557dfe850.js
@@ -0,0 +1,8 @@
+function showDiv(divID) {
+   if (document.getElementById(divID).style.display == "none") {
+   	document.getElementById(divID).style.display = "block";
+   } else {
+   	document.getElementById(divID).style.display = "none"
+   }
+}
+;
