ac06b2d2306f7eb49be7bbdf51596aa9c3ee9671
tannergilligan <tanner12@stanford.edu>
2015-05-28 23:05:32 -0700

    Fixed page

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150525042507) do
-
 
   create_table "agrees", force: true do |t|
     t.datetime "created_at"
